b359ac1d86ad8c43d41c151f97b5a32849c475ba
mobeets <mobeets@gmail.com>
2019-06-14 16:40:25 -0400

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+
+@tweeprint retweets all tweeprints, so that it's easier to come across them in your feed. This account will retweet threads that include "#tweeprint" in the first tweet of the thread, or as a reply to the first tweet in the thread.
+
+## Examples
+
+- https://twitter.com/avdnoord/status/1135900129402208257
+- https://twitter.com/DavidBeniaguev/status/1131890349578829825
+- https://twitter.com/jh_jacobsen/status/1110208461927841793
+- https://twitter.com/maxdonelan/status/1034880537175814144
+- https://twitter.com/Jeff_Weiler/status/1094994925052080130
+- https://twitter.com/jmacshine/status/1087439025588207616
+- https://twitter.com/NatashaMhatre/status/1074739264879874048
---
+++ main.py
@@ -0,0 +1,84 @@
+import os
+import tweepy
+
+CONSUMER_KEY = os.environ['TWITTER_CONSUMER_KEY']
+CONSUMER_SECRET = os.environ['TWITTER_CONSUMER_SECRET']
+OAUTH_TOKEN = os.environ['TWITTER_OAUTH_TOKEN']
+OAUTH_TOKEN_SECRET = os.environ['TWITTER_OAUTH_TOKEN_SECRET']
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+api = tweepy.API(auth)
+
+USER_NAME = 'tweeprint'
+HASHTAG = '#tweeprint'
+
+def get_last_tweet_id():
+    tweet = api.user_timeline(screen_name=USER_NAME, count=1)
+    if len(tweet) == 0:
+        return None
+    else:
+        return tweet[0].id
+
+def get_query():
+    return HASHTAG + ' -filter:retweets'
+
+def fetch_mentions(last_id, max_tweets=50):
+    query = get_query()
+    matches = []
+    for status in tweepy.Cursor(api.search, q=query,
+        count=max_tweets, # tweet_mode='extended',
+        since_id=last_id, result_type='recent').items(max_tweets):
+        print status.text
+        if not hasattr(status, 'retweeted_status'):
+            print 'Found status {}'.format(status.id)
+            matches.append(status)
+    return matches
+
+def get_tweeprints(tweets):
+    """
+    tweets contains statuses mentioning '#tweeprint'
+        each tweet may be the first in the thread,
+        or a reply to the first in the thread;
+        in the latter case, we need to get that first thread
+    """
+    matches = []
+    for tweet in tweets:
+        if tweet.in_reply_to_status_id_str:
+            prev_tweet = api.get_status(tweet.in_reply_to_status_id)
+            matches.append(prev_tweet)
+            print 'Adding previous status {}'.format(prev_tweet.id)
+        else:
+            matches.append(tweet)
+            print 'Adding status {}'.format(tweet.id)
+    return matches
+
+def retweet_tweeprints(tweets):
+    for tweet in tweets:
+        if tweet.in_reply_to_status_id_str:
+            print 'Ignoring {} because it is not the first in the thread'.format(tweet.id)
+            continue
+        if tweet.user.screen_name == USER_NAME:
+            print 'Ignoring my own tweet'
+            continue
+        urls = [url for url in tweet.entities['urls'] if 'twitter' not in url['display_url']]
+        # need to filter out by display_url to make sure we don't include links to other tweets (i.e., RTs) as a valid url
+        if len(urls) == 0:
+            print 'Ignoring {} because there are no urls'.format(tweet.id)
+            continue
+        if not tweet.retweeted:
+            try:
+                tweet.retweet()
+                print 'Retweeting {}'.format(tweet.id)
+            except tweepy.TweepError as e:
+                print 'Already Retweeted {}'.format(tweet.id)
+
+def main():
+    last_id = get_last_tweet_id()
+    print 'Last id = {}'.format(last_id)
+    tweets = fetch_mentions(last_id)
+    tweets = get_tweeprints(tweets)
+    retweet_tweeprints(tweets)
+
+if __name__ == '__main__':
+    main()
